f5a8775f133d9f4bf84680b43026b2c2bafe5389
Sarah Azeez <sarahazeez04@gmail.com>
2024-04-16 20:25:42 +0530

    Create charType.py

---
+++ charType.py
@@ -0,0 +1,7 @@
+ch=(input())
+if ch>='A' and ch<='Z':
+    print(ch,"is a upper case letter")
+elif ch>='a' and ch<='z':
+    print(ch,"is a lower case charecter")
+else:
+    print(ch,"is a special charecter or a number")
